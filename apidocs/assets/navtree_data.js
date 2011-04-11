e2cd555f8b1c8b618aba398e2445db64367e957b
simonetripodi <simone.tripodi@gmail.com>
2011-04-11 15:22:11 +0200

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,17 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.t2t", "org/nnsoft/t2t/package-summary.html", [ [ "Description", "org/nnsoft/t2t/package-descr.html", null, "" ], [ "Classes", null, [ [ "Runner", "org/nnsoft/t2t/Runner.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.t2t.configuration", "org/nnsoft/t2t/configuration/package-summary.html", [ [ "Description", "org/nnsoft/t2t/configuration/package-descr.html", null, "" ], [ "Classes", null, [ [ "ConfigurationManager", "org/nnsoft/t2t/configuration/ConfigurationManager.html", null, "" ], [ "MigratorConfiguration", "org/nnsoft/t2t/configuration/MigratorConfiguration.html", null, "" ], [ "MigratorConfiguration.ConnectionParameter", "org/nnsoft/t2t/configuration/MigratorConfiguration.ConnectionParameter.html", null, "" ], [ "NamespaceMapping", "org/nnsoft/t2t/configuration/NamespaceMapping.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.t2t.core", "org/nnsoft/t2t/core/package-summary.html", [ [ "Description", "org/nnsoft/t2t/core/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Migrator", "org/nnsoft/t2t/core/Migrator.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "DefaultMigrator", "org/nnsoft/t2t/core/DefaultMigrator.html", null, "" ], [ "MigrationStats", "org/nnsoft/t2t/core/MigrationStats.html", null, "" ], [ "Rule", "org/nnsoft/t2t/core/Rule.html", null, "" ], [ "RuleExecurionReport", "org/nnsoft/t2t/core/RuleExecurionReport.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "MigratorException", "org/nnsoft/t2t/core/MigratorException.html", null, "" ], [ "RuleExecutionException", "org/nnsoft/t2t/core/RuleExecutionException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.t2t.slicers", "org/nnsoft/t2t/slicers/package-summary.html", [ [ "Description", "org/nnsoft/t2t/slicers/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Slicer", "org/nnsoft/t2t/slicers/Slicer.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "FonteRSSSlicer", "org/nnsoft/t2t/slicers/FonteRSSSlicer.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "SlicerException", "org/nnsoft/t2t/slicers/SlicerException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,22 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.t2t", link:"org/nnsoft/t2t/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.t2t.Runner", link:"org/nnsoft/t2t/Runner.html", type:"class" },
+      { id:2, label:"org.nnsoft.t2t.configuration", link:"org/nnsoft/t2t/configuration/package-summary.html", type:"package" },
+      { id:3, label:"org.nnsoft.t2t.configuration.ConfigurationManager", link:"org/nnsoft/t2t/configuration/ConfigurationManager.html", type:"class" },
+      { id:4, label:"org.nnsoft.t2t.configuration.MigratorConfiguration", link:"org/nnsoft/t2t/configuration/MigratorConfiguration.html", type:"class" },
+      { id:5, label:"org.nnsoft.t2t.configuration.MigratorConfiguration.ConnectionParameter", link:"org/nnsoft/t2t/configuration/MigratorConfiguration.ConnectionParameter.html", type:"class" },
+      { id:6, label:"org.nnsoft.t2t.configuration.NamespaceMapping", link:"org/nnsoft/t2t/configuration/NamespaceMapping.html", type:"class" },
+      { id:7, label:"org.nnsoft.t2t.core", link:"org/nnsoft/t2t/core/package-summary.html", type:"package" },
+      { id:8, label:"org.nnsoft.t2t.core.DefaultMigrator", link:"org/nnsoft/t2t/core/DefaultMigrator.html", type:"class" },
+      { id:9, label:"org.nnsoft.t2t.core.MigrationStats", link:"org/nnsoft/t2t/core/MigrationStats.html", type:"class" },
+      { id:10, label:"org.nnsoft.t2t.core.Migrator", link:"org/nnsoft/t2t/core/Migrator.html", type:"class" },
+      { id:11, label:"org.nnsoft.t2t.core.MigratorException", link:"org/nnsoft/t2t/core/MigratorException.html", type:"class" },
+      { id:12, label:"org.nnsoft.t2t.core.Rule", link:"org/nnsoft/t2t/core/Rule.html", type:"class" },
+      { id:13, label:"org.nnsoft.t2t.core.RuleExecurionReport", link:"org/nnsoft/t2t/core/RuleExecurionReport.html", type:"class" },
+      { id:14, label:"org.nnsoft.t2t.core.RuleExecutionException", link:"org/nnsoft/t2t/core/RuleExecutionException.html", type:"class" },
+      { id:15, label:"org.nnsoft.t2t.slicers", link:"org/nnsoft/t2t/slicers/package-summary.html", type:"package" },
+      { id:16, label:"org.nnsoft.t2t.slicers.FonteRSSSlicer", link:"org/nnsoft/t2t/slicers/FonteRSSSlicer.html", type:"class" },
+      { id:17, label:"org.nnsoft.t2t.slicers.Slicer", link:"org/nnsoft/t2t/slicers/Slicer.html", type:"class" },
+      { id:18, label:"org.nnsoft.t2t.slicers.SlicerException", link:"org/nnsoft/t2t/slicers/SlicerException.html", type:"class" }
+
+    ];
